e3914959c254b23f130ba8326a9c16982c20aecc
C.W. Betts <computers57@hotmail.com>
2014-08-29 13:17:10 -0600

    Initial support for going through an XPC array via enumerate functions.

---
+++ SwiftXPC/Support/Generators.swift
@@ -0,0 +1,29 @@
+//
+//  Generators.swift
+//  SwiftXPC
+//
+//  Created by C.W. Betts on 8/29/14.
+//  Copyright (c) 2014 William Kent. All rights reserved.
+//
+
+import Foundation
+import XPC
+
+public struct XPCArrayGenerator: GeneratorType {
+	private let theArray: XPCArray
+	private var status: Int
+	public init(array: XPCArray) {
+		status = 0
+		theArray = array
+	}
+	
+	mutating public func next() -> XPCObject? {
+		return nil
+	}
+}
+
+extension XPCArray: SequenceType {
+	public func generate() -> XPCArrayGenerator {
+		return XPCArrayGenerator(array: self)
+	}
+}
